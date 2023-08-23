cca32ba163ba13f1280ec58a31622b7de7ff1d8f
karnan7 <adithkayalad@gmail.com>
2023-08-23 23:53:15 +0530

    insert node function

--- src/hooks/useFileTree.js
+++ src/hooks/useFileTree.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const useFileTree = () => {
 
-  const insertNode = () => {
+  const insertNode = (isFolder) => {
     
   };
+
   return (
     <div>useFileTree</div>
   )
